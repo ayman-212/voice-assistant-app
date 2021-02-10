f38c4b642003c13e247774ca07c0c09bf6e2748a
Ayman Abdelazez <aymanaea52@gmail.com>
2021-02-10 02:45:41 +0200

    alan button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import alanBtn from "@alan-ai/alan-sdk-web";
 
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+const App = () => {
+    useEffect(() => {
+        alanBtn({
+            key: '692ac4c6806c911ff0188fa10729f6b32e956eca572e1d8b807a3e2338fdd0dc/stage',
+            onCommand: ({ command, articles }) => {
+                if (command === 'newsHeadlines') {
+                    console.log(articles)
+                }
+            }
+        });
+    }, []);
+    return (
+        <div >
+            <h1>Alan AI news application</h1>
+        </div>
+    );
 }
 
 export default App;
