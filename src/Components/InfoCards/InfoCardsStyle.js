87465ab3a7d50bf7574e8d1a5ad7a55a27aa6e78
Ayman Abdelazez <aymanaea52@gmail.com>
2021-02-13 01:19:25 +0200

    infoCard Component

---
+++ src/Components/InfoCards/InfoCardsStyle.js
@@ -0,0 +1,14 @@
+import { makeStyles } from '@material-ui/core';
+
+export default makeStyles({
+    main: {
+        width: '100%',
+        margin: '10px 0',
+        padding: '0 5%'
+    },
+    info: {
+        display: 'flex',
+        flexDirection: 'column',
+        textAlign: 'center'
+    }
+})
